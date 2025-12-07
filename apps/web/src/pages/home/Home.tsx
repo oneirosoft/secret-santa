b496faaa6266767db7a4e443ae2be7e92ab65c28
Mark <20671988+mark-pro@users.noreply.github.com>
2025-12-07 10:02:32 -0500

    feat: created input element with holiday theme (#12)

--- apps/web/src/pages/home/Home.tsx
+++ apps/web/src/pages/home/Home.tsx
@@ -1,10 +1,12 @@
 
 import Countdown from './Countdown'
+import Input from '@/components/Input';
 import './home.css';
 
 const Home = () => {
-
-  return <div className='home'><h1>Secret Santa</h1><Countdown /></div>
+  return <div className='home'><h1>Secret Santa</h1>
+  <Input name='workshop-phrase' type='text' fullWidth label='Workshop Phrase' />
+  <Countdown /></div>
 }
 
 export default Home
